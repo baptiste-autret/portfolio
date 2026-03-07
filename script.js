9fc7216f04f5f9e27f7b647d1de82d06a6ef4834
Baptiste A. <shadowbaba.contact@gmail.com>
2026-03-07 15:43:59 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const textes = [
+  `Étudiant en <a href="https://fr.wikipedia.org/wiki/Brevet_de_technicien_sup%C3%A9rieur_-_Services_informatiques_aux_organisations"
+     target="_blank" class="text-decoration-none">BTS SIO</a> option
+   <a href="https://www.onisep.fr/ressources/univers-formation/formations/post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers"
+     target="_blank" class="text-decoration-none">SLAM</a>`,
+  "- En recherche d'un stage du <strong>5 janvier</strong> au <strong>13 février 2026</strong> -"
+];
+
+const delays = [8000, 2000];
+
+let i = 0;
+
+function switchText() {
+  document.getElementById("description").innerHTML = textes[i];
+  i = (i + 1) % textes.length;
+  setTimeout(switchText, delays[i]);
+}
+
+switchText();
+
+function scrollToSection(sectionId) {
+    
+    const section = document.getElementById(sectionId);
+    
+    window.scrollTo({
+        top: section.offsetTop - 65,
+        behavior: 'smooth'
+    });
+}
